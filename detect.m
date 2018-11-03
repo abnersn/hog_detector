651be68747e3da4ff65273dca8b39a703398a32c
Abner <abnersousanascimento@gmail.com>
2018-11-03 01:53:46 -0300

    organização

--- detect.m
+++ detect.m
@@ -4,10 +4,10 @@ BLOCK_SIZE = [32 32];
 PATCH_SIZE = 4;
 BINS = 8;
 NORM_KERNEL_SIZE = 2;
-DISPLAY = false;
+DISPLAY = true;
 STEP = 2;
 
-I = rgb2gray(imread('sw.jpg'));
+I = rgb2gray(imread('samples/sample.jpg'));
 
 load('svm_model.mat', 'svm_model');
 [m,p,g] = gradient(I);
@@ -17,8 +17,8 @@ last_x_index = BLOCK_SIZE(2) * (floor(size(I, 2) / BLOCK_SIZE(2)) - 1);
 x_range = 1:STEP:last_x_index;
 y_range = 1:STEP:last_y_index;
 
-figure;
-I_disp = I;
+f = figure;
+I_disp = cat(3, I, I, I);
 fprintf("Processando");
 for i = y_range
     for j = x_range
@@ -28,7 +28,7 @@ for i = y_range
         if DISPLAY
             imshow(I_disp);
             hold on;
-            rectangle('Position',[j i 32 32]);
+            rectangle('Position',[j i BLOCK_SIZE], 'EdgeColor', 'g');
             pause(0.0001);
         end
         
@@ -36,7 +36,17 @@ for i = y_range
         hist = hog(block, PATCH_SIZE, BINS, NORM_KERNEL_SIZE);
         [is_face, score] = predict(svm_model, hist(:)');
         if is_face == 1 && max(score) > 0.5
-            I_disp(b_ver, b_hor) = I_disp(b_ver, b_hor) * 0.5;
+            if DISPLAY
+                % Desenha um retângulo amarelo
+                I_disp(b_ver, j, :) = 255;
+                I_disp(b_ver, j, 3) = 0;
+                I_disp(b_ver, j + BLOCK_SIZE(2), :) = 255;
+                I_disp(b_ver, j + BLOCK_SIZE(2), 3) = 0;
+                I_disp(i, b_hor, :) = 255;
+                I_disp(i, b_hor, 3) = 0;
+                I_disp(i + BLOCK_SIZE(1), b_hor, :) = 255;
+                I_disp(i + BLOCK_SIZE(1), b_hor, 3) = 0;
+            end
         end
     end
 end
